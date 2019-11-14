bb6ede29e92c8a124819e019a851655def1ca1ad
Abel Katona <katona.abel@gmail.com>
2019-11-14 12:45:17 +0100

    gitlabci fix

--- Resources/gitlab_ci/create_composer.py
+++ Resources/gitlab_ci/create_composer.py
@@ -2,11 +2,15 @@ import json
 import os
 import sys
 
+print("Version:",sys.argv[1])
+
 path = os.path.dirname(os.path.realpath(__file__))
 with open(path+'/../../composer.json', 'r+') as f:
     data = json.load(f)
-    for key in data['require']:
-    	if key.startswith('ymfony/'):
-    		data['require'][key] = sys.argv[1]
-    with open(path+'/../../custom_composer.json', 'w') as outfile:
-    	json.dump(data, outfile, indent=4)
+
+for key in data['require']:
+    if key.startswith('ymfony/'):
+        data['require'][key] = sys.argv[1]
+
+with open(path+'/../../composer.json', 'w') as outfile:
+    json.dump(data, outfile, indent=4)
